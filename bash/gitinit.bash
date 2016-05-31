ff3f3ec0c3fe4f189ca5b4f6bf1c43f7c80bd17b
javascriptiscoolpl <javascriptiscoolpl@users.noreply.github.com>
2016-05-31 14:16:31 +0200

    improve git

--- bash/gitinit.bash
+++ bash/gitinit.bash
@@ -1,6 +1,8 @@
 PROJECT="https://github.com/javascriptiscoolpl/graphQL-tinySQL.git"
 
-git init; git add .;
+git init;
+git add .;
 git commit -m \"init\";
 git remote add origin "${PROJECT}";
+echo -e "Username: javascriptiscoolpl";
 git push -u origin master;
